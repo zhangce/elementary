a64933a5a8be1661432cc5a8bb4f83e76e89c71e
Ce Zhang <czhang@cs.wisc.edu>
2012-07-04 12:54:43 -0500

    init commit for elly.

---
+++ elly/elly/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  elly
+//
+//  Created by Ce Zhang on 7/4/12.
+//  Copyright (c) 2012 University of Wisconsin-Madison. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
