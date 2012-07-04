4121a7a891b7f02171df8276085311124139cb3b
Ce Zhang <czhang@cs.wisc.edu>
2012-07-04 18:52:46 -0500

    before unit test

---
+++ elly/elly_test/elly_test.h
@@ -0,0 +1,13 @@
+//
+//  elly_test.h
+//  elly_test
+//
+//  Created by Ce Zhang on 7/4/12.
+//  Copyright (c) 2012 University of Wisconsin-Madison. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface elly_test : SenTestCase
+
+@end
